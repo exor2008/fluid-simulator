26420883af8ecb4efdf8586e1e4ba24d752862b3
Ian <exor2008@yandex.ru>
2023-12-08 21:56:22 -0500

    Compare cpu and gpu

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use cudarc::{driver::*, nvrtc::compile_ptx};
+use rand::prelude::*;
+use std::time::Instant;
 
 fn main() -> Result<(), DriverError> {
     let dev = CudaDevice::new(0)?;
@@ -11,12 +13,26 @@ fn main() -> Result<(), DriverError> {
 
     let f = dev.get_func("sin", "sin_kernel").unwrap();
 
-    let a_host = [1.0f32, 2.0, 3.0];
+    let mut a_host = vec![0f32; 360_000_000];
+    let mut rng = rand::thread_rng();
+    for i in 0..a_host.len() {
+        a_host[i] = rng.gen();
+    }
 
+    let start_time = Instant::now();
+
+    let mut result = vec![0f32; 360_000_000];
+    for i in 0..a_host.len() {
+        result[i] = a_host[i].sin();
+    }
+    let elapsed_time = start_time.elapsed();
+    println!("CPU: {:?}", elapsed_time);
+
+    let start_time = Instant::now();
     let a_dev = dev.htod_copy(a_host.into())?;
     let mut b_dev = a_dev.clone();
 
-    let n = 3;
+    let n = 360_000_000;
     let cfg = LaunchConfig::for_num_elems(n);
 
     unsafe { f.launch(cfg, (&mut b_dev, &a_dev, n as i32)) }?;
@@ -24,9 +40,14 @@ fn main() -> Result<(), DriverError> {
     let a_host_2 = dev.sync_reclaim(a_dev)?;
     let b_host = dev.sync_reclaim(b_dev)?;
 
-    println!("Found {:?}", b_host);
-    println!("Expected {:?}", a_host.map(f32::sin));
-    assert_eq!(&a_host, a_host_2.as_slice());
+    let elapsed_time = start_time.elapsed();
+
+    // println!("Found {:?}", b_host);
+    println!("GPU {:?}", elapsed_time);
+    println!("CPU {:?}", result[1000]);
+    println!("GPU {:?}", b_host[1000]);
+    // println!("Expected {:?}", a_host.map(f32::sin));
+    // assert_eq!(result, b_host);
 
     Ok(())
 }
