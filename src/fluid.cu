3ea875b65b0746a1c4682d674a635ab709a2a03a
Ian <exor2008@yandex.ru>
2023-12-10 00:34:54 -0500

    GPU: Projection

--- src/fluid.cu
+++ src/fluid.cu
@@ -5,6 +5,39 @@ extern "C" __global__ void divergence(float *div, const float *u, const float *v
     if (x > 0 && y > 0 && x < cols - 1 && y < rows - 1)
     {
         int idx = y * cols + x;
-        div[idx] = u[y * cols + x + 1] - u[idx] + v[(y + 1) * cols + x + 1] - v[idx];
+        float du_dx = u[y * cols + x + 1] - u[idx];
+        float dv_dy = v[(y + 1) * cols + x + 1] - v[idx];
+        div[idx] = du_dx + dv_dy;
+    }
+}
+
+extern "C" __global__ void pressure(float *pressure_a, float *pressure_b, const float *div, int rows, int cols)
+{
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (x > 0 && y > 0 && x < cols - 1 && y < rows - 1)
+    {
+        int idx = y * cols + x;
+
+        float sum = pressure_b[(y + 1) * cols + x] + pressure_b[(y - 1) * cols + x] + pressure_b[y * cols + x + 1] + pressure_b[y * cols + x - 1];
+        pressure_a[idx] = (sum - div[idx]) / 4;
+    }
+}
+
+extern "C" __global__ void incompress(float *u, float *v, const float *pressure, int rows, int cols)
+{
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (x > 0 && y > 0 && x < cols - 1 && y < rows - 1)
+    {
+        int idx = y * cols + x;
+
+        float dp_dx = pressure[y * cols + x + 1] - pressure[idx];
+        float dp_dy = pressure[(y + 1) * cols + x] - pressure[idx];
+
+        u[idx] -= dp_dx;
+        v[idx] -= dp_dy;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::mem::swap;
+
 use cudarc::{driver::*, nvrtc::compile_ptx};
 
 const ROWS: usize = 100;
 const COLS: usize = 100;
 const SIZE: usize = ROWS * COLS;
+const ITERATIONS: usize = 40;
 
 fn main() -> Result<(), DriverError> {
     let dev = CudaDevice::new(0)?;
@@ -11,13 +14,13 @@ fn main() -> Result<(), DriverError> {
 
     let ptx = compile_ptx(PTX_SRC).unwrap();
 
-    dev.load_ptx(ptx, "fluid", &["divergence"])?;
-
-    let divergence = dev.get_func("fluid", "divergence").unwrap();
+    dev.load_ptx(ptx, "fluid", &["divergence", "pressure", "incompress"])?;
 
     let mut u_host = vec![0f32; SIZE];
     let v_host = vec![0f32; SIZE];
     let div_host = vec![0f32; SIZE];
+    let pressure_a_host = vec![0f32; SIZE];
+    let pressure_b_host = vec![0f32; SIZE];
 
     for y in 0..ROWS {
         for x in 0..COLS {
@@ -27,9 +30,11 @@ fn main() -> Result<(), DriverError> {
         }
     }
 
-    let u_dev = dev.htod_copy(u_host.into())?;
-    let v_dev = dev.htod_copy(v_host.into())?;
+    let mut u_dev = dev.htod_copy(u_host.into())?;
+    let mut v_dev = dev.htod_copy(v_host.into())?;
     let mut div_dev = dev.htod_copy(div_host.into())?;
+    let mut pressure_a_dev = dev.htod_copy(pressure_a_host.into())?;
+    let mut pressure_b_dev = dev.htod_copy(pressure_b_host.into())?;
 
     let cfg = LaunchConfig {
         grid_dim: (10, 10, 1),
@@ -37,13 +42,39 @@ fn main() -> Result<(), DriverError> {
         shared_mem_bytes: 0,
     };
 
-    unsafe { divergence.launch(cfg, (&mut div_dev, &u_dev, &v_dev, ROWS, COLS)) }?;
+    unsafe {
+        // Divergence
+        let divergence = dev.get_func("fluid", "divergence").unwrap();
+        divergence.launch(cfg, (&mut div_dev, &u_dev, &v_dev, ROWS, COLS))?;
 
-    let div_host = dev.sync_reclaim(div_dev)?;
+        // Pressure Jacobi
+        for _ in 0..ITERATIONS {
+            let pressure = dev.get_func("fluid", "pressure").unwrap();
+
+            pressure.launch(
+                cfg,
+                (
+                    &mut pressure_a_dev,
+                    &mut pressure_b_dev,
+                    &div_dev,
+                    ROWS,
+                    COLS,
+                ),
+            )?;
+
+            swap(&mut pressure_a_dev, &mut pressure_b_dev);
+        }
+
+        // Solve Incompressibility
+        let incompress = dev.get_func("fluid", "incompress").unwrap();
+        incompress.launch(cfg, (&mut u_dev, &mut v_dev, &pressure_a_dev))?;
+    };
+
+    let result = dev.sync_reclaim(u_dev)?;
 
     for y in 0..ROWS {
         for x in 0..COLS {
-            print!("{}", div_host[y * COLS + x]);
+            print!("{}", result[y * COLS + x]);
         }
         println!();
     }
