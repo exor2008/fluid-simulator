685910cc3fcb8c277340d4f835728b280bdb7174
Ian <exor2008@yandex.ru>
2023-12-10 23:59:23 -0500

    struct Fluid

---
+++ src/lib.rs
@@ -0,0 +1,170 @@
+use cudarc::driver::*;
+use std::{mem::swap, sync::Arc};
+
+const ITERATIONS: usize = 40;
+const H: f32 = 1.0 / 100.0;
+
+pub struct Fluid {
+    u_dev: CudaSlice<f32>,
+    v_dev: CudaSlice<f32>,
+    new_u_dev: CudaSlice<f32>,
+    new_v_dev: CudaSlice<f32>,
+    smoke_dev: CudaSlice<f32>,
+    new_smoke_dev: CudaSlice<f32>,
+    div_dev: CudaSlice<f32>,
+    pressure_a_dev: CudaSlice<f32>,
+    pressure_b_dev: CudaSlice<f32>,
+    rows: usize,
+    cols: usize,
+}
+
+impl Fluid {
+    pub fn new(dev: Arc<CudaDevice>, rows: usize, cols: usize) -> Result<Self, DriverError> {
+        let size = rows * cols;
+
+        let mut u_host = vec![0f32; size];
+        let v_host = vec![0f32; size];
+        let new_u_host = vec![0f32; size];
+        let new_v_host = vec![0f32; size];
+        let mut smoke_host = vec![0f32; size];
+        let new_smoke_host = vec![0f32; size];
+        let div_host = vec![0f32; size];
+        let pressure_a_host = vec![0f32; size];
+        let pressure_b_host = vec![0f32; size];
+
+        for y in 0..rows {
+            for x in 0..cols {
+                if x == 5 {
+                    u_host[y * cols + x] = 2.0;
+                    smoke_host[y * cols + x] = 1.0;
+                }
+            }
+        }
+
+        let u_dev = dev.htod_copy(u_host)?;
+        let v_dev = dev.htod_copy(v_host)?;
+        let new_u_dev = dev.htod_copy(new_u_host)?;
+        let new_v_dev = dev.htod_copy(new_v_host)?;
+        let smoke_dev = dev.htod_copy(smoke_host)?;
+        let new_smoke_dev = dev.htod_copy(new_smoke_host)?;
+        let div_dev = dev.htod_copy(div_host)?;
+        let pressure_a_dev = dev.htod_copy(pressure_a_host)?;
+        let pressure_b_dev = dev.htod_copy(pressure_b_host)?;
+
+        let fluid = Fluid {
+            u_dev,
+            v_dev,
+            new_u_dev,
+            new_v_dev,
+            smoke_dev,
+            new_smoke_dev,
+            div_dev,
+            pressure_a_dev,
+            pressure_b_dev,
+            rows,
+            cols,
+        };
+
+        Ok(fluid)
+    }
+
+    pub fn step(
+        &mut self,
+        dev: Arc<CudaDevice>,
+        cfg: LaunchConfig,
+        dt: f32,
+    ) -> Result<(), DriverError> {
+        unsafe {
+            // Divergence
+            let divergence = dev.get_func("fluid", "divergence").unwrap();
+            divergence.launch(
+                cfg,
+                (
+                    &mut self.div_dev,
+                    &self.u_dev,
+                    &self.v_dev,
+                    self.rows,
+                    self.cols,
+                ),
+            )?;
+            dev.synchronize()?;
+
+            // Pressure Jacobi
+            for _ in 0..ITERATIONS {
+                let pressure = dev.get_func("fluid", "pressure").unwrap();
+
+                pressure.launch(
+                    cfg,
+                    (
+                        &mut self.pressure_a_dev,
+                        &mut self.pressure_b_dev,
+                        &self.div_dev,
+                        self.rows,
+                        self.cols,
+                    ),
+                )?;
+                dev.synchronize()?;
+
+                swap(&mut self.pressure_a_dev, &mut self.pressure_b_dev);
+            }
+
+            // Solve Incompressibility
+            let incompress = dev.get_func("fluid", "incompress").unwrap();
+            incompress.launch(
+                cfg,
+                (
+                    &mut self.u_dev,
+                    &mut self.v_dev,
+                    &self.pressure_a_dev,
+                    self.rows,
+                    self.cols,
+                ),
+            )?;
+            dev.synchronize()?;
+
+            // Advect velocity
+            let advect_velocity = dev.get_func("fluid", "advect_velocity").unwrap();
+            advect_velocity.launch(
+                cfg,
+                (
+                    &self.u_dev,
+                    &self.v_dev,
+                    &mut self.new_u_dev,
+                    &mut self.new_v_dev,
+                    &self.smoke_dev,
+                    dt,
+                    H,
+                    self.rows,
+                    self.cols,
+                ),
+            )?;
+            swap(&mut self.u_dev, &mut self.new_u_dev);
+            swap(&mut self.v_dev, &mut self.new_v_dev);
+            dev.synchronize()?;
+
+            // Advect smoke
+            let advect_smoke = dev.get_func("fluid", "advect_smoke").unwrap();
+            advect_smoke.launch(
+                cfg,
+                (
+                    &self.smoke_dev,
+                    &mut self.new_smoke_dev,
+                    &self.u_dev,
+                    &self.v_dev,
+                    dt,
+                    H,
+                    self.rows,
+                    self.cols,
+                ),
+            )?;
+            swap(&mut self.smoke_dev, &mut self.new_smoke_dev);
+        };
+        Ok(())
+    }
+
+    pub fn smoke(&self, dev: Arc<CudaDevice>) -> Result<Vec<f32>, DriverError> {
+        let result = dev.sync_reclaim(self.smoke_dev.clone())?;
+
+        Ok(result)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
-use std::mem::swap;
-
 use cudarc::{driver::*, nvrtc::compile_ptx};
+use fluid_simulator::Fluid;
 
 const ROWS: usize = 100;
 const COLS: usize = 100;
-const SIZE: usize = ROWS * COLS;
-const ITERATIONS: usize = 40;
-const H: f32 = 1.0 / 100.0;
 const DT: f32 = 0.01;
 
 fn main() -> Result<(), DriverError> {
@@ -28,110 +24,25 @@ fn main() -> Result<(), DriverError> {
         ],
     )?;
 
-    let mut u_host = vec![0f32; SIZE];
-    let v_host = vec![0f32; SIZE];
-    let new_u_host = vec![0f32; SIZE];
-    let new_v_host = vec![0f32; SIZE];
-    let mut smoke_host = vec![0f32; SIZE];
-    let new_smoke_host = vec![0f32; SIZE];
-    let div_host = vec![0f32; SIZE];
-    let pressure_a_host = vec![0f32; SIZE];
-    let pressure_b_host = vec![0f32; SIZE];
-
-    for y in 0..ROWS {
-        for x in 0..COLS {
-            if x == 5 {
-                u_host[y * COLS + x] = 2.0;
-                smoke_host[y * COLS + x] = 1.0;
-            }
-        }
-    }
-
-    let mut u_dev = dev.htod_copy(u_host.into())?;
-    let mut v_dev = dev.htod_copy(v_host.into())?;
-    let mut new_u_dev = dev.htod_copy(new_u_host.into())?;
-    let mut new_v_dev = dev.htod_copy(new_v_host.into())?;
-    let mut smoke_dev = dev.htod_copy(smoke_host.into())?;
-    let mut new_smoke_dev = dev.htod_copy(new_smoke_host.into())?;
-    let mut div_dev = dev.htod_copy(div_host.into())?;
-    let mut pressure_a_dev = dev.htod_copy(pressure_a_host.into())?;
-    let mut pressure_b_dev = dev.htod_copy(pressure_b_host.into())?;
-
     let cfg = LaunchConfig {
         grid_dim: (10, 10, 1),
         block_dim: (10, 10, 1),
         shared_mem_bytes: 0,
     };
 
-    unsafe {
-        // Divergence
-        let divergence = dev.get_func("fluid", "divergence").unwrap();
-        divergence.launch(cfg, (&mut div_dev, &u_dev, &v_dev, ROWS, COLS))?;
-        dev.synchronize()?;
+    let mut fluid = Fluid::new(dev.clone(), ROWS, COLS).unwrap();
+    fluid.step(dev.clone(), cfg, DT)?;
+    let result = fluid.smoke(dev.clone())?;
 
-        // Pressure Jacobi
-        for _ in 0..ITERATIONS {
-            let pressure = dev.get_func("fluid", "pressure").unwrap();
-
-            pressure.launch(
-                cfg,
-                (
-                    &mut pressure_a_dev,
-                    &mut pressure_b_dev,
-                    &div_dev,
-                    ROWS,
-                    COLS,
-                ),
-            )?;
-            dev.synchronize()?;
-
-            swap(&mut pressure_a_dev, &mut pressure_b_dev);
+    for y in 0..ROWS {
+        for x in 0..COLS {
+            print!("{}", result[y * COLS + x]);
         }
+        println!();
+    }
 
-        // Solve Incompressibility
-        let incompress = dev.get_func("fluid", "incompress").unwrap();
-        incompress.launch(cfg, (&mut u_dev, &mut v_dev, &pressure_a_dev, ROWS, COLS))?;
-        dev.synchronize()?;
-
-        // Advect velocity
-        let advect_velocity = dev.get_func("fluid", "advect_velocity").unwrap();
-        advect_velocity.launch(
-            cfg,
-            (
-                &u_dev,
-                &v_dev,
-                &mut new_u_dev,
-                &mut new_v_dev,
-                &smoke_dev,
-                DT,
-                H,
-                ROWS,
-                COLS,
-            ),
-        )?;
-        swap(&mut u_dev, &mut new_u_dev);
-        swap(&mut v_dev, &mut new_v_dev);
-        dev.synchronize()?;
-
-        // Advect smoke
-        let advect_smoke = dev.get_func("fluid", "advect_smoke").unwrap();
-        advect_smoke.launch(
-            cfg,
-            (
-                &smoke_dev,
-                &mut new_smoke_dev,
-                &u_dev,
-                &v_dev,
-                DT,
-                H,
-                ROWS,
-                COLS,
-            ),
-        )?;
-        swap(&mut smoke_dev, &mut new_smoke_dev);
-    };
-
-    let result = dev.sync_reclaim(smoke_dev)?;
+    fluid.step(dev.clone(), cfg, DT)?;
+    let result = fluid.smoke(dev.clone())?;
 
     for y in 0..ROWS {
         for x in 0..COLS {
