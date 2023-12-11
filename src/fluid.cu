055e0e8578e87ade008d5d47820cc1d6785aa154
Ian <exor2008@yandex.ru>
2023-12-10 22:52:52 -0500

    GPU: Advect vel and smoke

--- src/fluid.cu
+++ src/fluid.cu
@@ -1,3 +1,7 @@
+__constant__ int U_FEILD = 0;
+__constant__ int V_FEILD = 1;
+__constant__ int S_FEILD = 2;
+
 extern "C" __global__ void divergence(float *div, const float *u, const float *v, int rows, int cols)
 {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
@@ -39,5 +43,143 @@ extern "C" __global__ void incompress(float *u, float *v, const float *pressure,
 
         u[idx] -= dp_dx;
         v[idx] -= dp_dy;
+    }
+}
+
+extern "C" __device__ float avg_u(const float *u, int rows, int cols)
+{
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (x > 0 && y > 0 && x < cols - 1 && y < rows - 1)
+    {
+        // ***
+        // *++
+        // *++
+        return (u[(y - 1) * cols + x] + u[y * cols + x] + u[(y - 1) * cols + x + 1] + u[y * cols + x + 1]) * 0.25;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+extern "C" __device__ float avg_v(const float *v, int rows, int cols)
+{
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (x > 0 && y > 0 && x < cols - 1 && y < rows - 1)
+    {
+        // ++*
+        // ++*
+        // ***
+        return (v[y * cols + x - 1] + v[y * cols + x] + v[(y + 1) * cols + x - 1] + v[(y + 1) * cols + x]) * 0.25;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+extern "C" __device__ float sample_field(const float *u, const float *v, const float *smoke, float x_shift, float y_shift, const int field_type, float h, int rows, int cols)
+{
+    float h1 = 1.0 / h;
+    float h2 = 0.5 * h;
+
+    x_shift = max(min(x_shift, cols * h), h);
+    y_shift = max(min(y_shift, rows * h), h);
+
+    float dx = 0.0;
+    float dy = 0.0;
+    const float *field;
+
+    if (field_type == 0)
+    {
+        field = u;
+        dy = h2;
+    }
+    else if (field_type == 1)
+    {
+        field = v;
+        dx = h2;
+    }
+    else
+    {
+        field = smoke;
+        dx = h2;
+        dy = h2;
+    }
+
+    float col = float(cols - 1);
+    float row = float(rows - 1);
+
+    float x0 = min(floor((x_shift - dx) * h1), col);
+    float tx = ((x_shift - dx) - x0 * h) * h1;
+    float x1 = min(x0 + 1, col);
+
+    float y0 = min(floor((y_shift - dy) * h1), row);
+    float ty = ((y_shift - dy) - y0 * h) * h1;
+    float y1 = min(y0 + 1, row);
+
+    float sx = 1.0 - tx;
+    float sy = 1.0 - ty;
+
+    int x_0 = int(x0);
+    int x_1 = int(x1);
+    int y_0 = int(y0);
+    int y_1 = int(y1);
+
+    return (
+        sx * sy * field[y_0 * cols + x_0] + tx * sy * field[y_0 * cols + x_1] + tx * ty * field[y_1 * cols + x_1] + sx * ty * field[y_0 * cols + x_1]);
+}
+
+extern "C" __global__ void advect_velocity(const float *u, const float *v, float *new_u, float *new_v, const float *smoke, float dt, float h, int rows, int cols)
+{
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (x > 0 && y > 0 && x < cols - 1 && y < rows - 1)
+    {
+        float h2 = 0.5 * h;
+
+        float x_shift = x * h;
+        float y_shift = y * h + h2;
+
+        float u_scalar = u[y * cols + x];
+        float v_scalar = avg_v(v, rows, cols);
+        x_shift -= dt * u_scalar;
+        y_shift -= dt * v_scalar;
+
+        new_u[y * cols + x] = sample_field(u, v, smoke, x_shift, y_shift, U_FEILD, h, rows, cols);
+
+        x_shift = x * h + h2;
+        y_shift = y * h;
+
+        u_scalar = avg_u(u, rows, cols);
+        v_scalar = v[y * cols + x];
+        x_shift -= dt * u_scalar;
+        y_shift -= dt * v_scalar;
+
+        new_v[y * cols + x] = sample_field(u, v, smoke, x_shift, y_shift, V_FEILD, h, rows, cols);
+    }
+}
+
+extern "C" __global__ void advect_smoke(const float *smoke, float *new_smoke, const float *u, const float *v, float dt, float h, int rows, int cols)
+{
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (x > 0 && y > 0 && x < cols - 1 && y < rows - 1)
+    {
+        float h2 = 0.5 * h;
+
+        float u_scalar = (u[y * cols + x] + u[y * cols + x + 1]) * 0.5;
+        float v_scalar = (v[y * cols + x] + v[(y + 1) * cols + x]) * 0.5;
+        float x_shift = x * h + h2 - dt * u_scalar;
+        float y_shift = y * h + h2 - dt * v_scalar;
+
+        float val = sample_field(u, v, smoke, x_shift, y_shift, S_FEILD, h, rows, cols);
+        new_smoke[y * cols + x] = val;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ const ROWS: usize = 100;
 const COLS: usize = 100;
 const SIZE: usize = ROWS * COLS;
 const ITERATIONS: usize = 40;
+const H: f32 = 1.0 / 100.0;
+const DT: f32 = 0.01;
 
 fn main() -> Result<(), DriverError> {
     let dev = CudaDevice::new(0)?;
@@ -14,10 +16,24 @@ fn main() -> Result<(), DriverError> {
 
     let ptx = compile_ptx(PTX_SRC).unwrap();
 
-    dev.load_ptx(ptx, "fluid", &["divergence", "pressure", "incompress"])?;
+    dev.load_ptx(
+        ptx,
+        "fluid",
+        &[
+            "divergence",
+            "pressure",
+            "incompress",
+            "advect_velocity",
+            "advect_smoke",
+        ],
+    )?;
 
     let mut u_host = vec![0f32; SIZE];
     let v_host = vec![0f32; SIZE];
+    let new_u_host = vec![0f32; SIZE];
+    let new_v_host = vec![0f32; SIZE];
+    let mut smoke_host = vec![0f32; SIZE];
+    let new_smoke_host = vec![0f32; SIZE];
     let div_host = vec![0f32; SIZE];
     let pressure_a_host = vec![0f32; SIZE];
     let pressure_b_host = vec![0f32; SIZE];
@@ -26,12 +42,17 @@ fn main() -> Result<(), DriverError> {
         for x in 0..COLS {
             if x == 5 {
                 u_host[y * COLS + x] = 2.0;
+                smoke_host[y * COLS + x] = 1.0;
             }
         }
     }
 
     let mut u_dev = dev.htod_copy(u_host.into())?;
     let mut v_dev = dev.htod_copy(v_host.into())?;
+    let mut new_u_dev = dev.htod_copy(new_u_host.into())?;
+    let mut new_v_dev = dev.htod_copy(new_v_host.into())?;
+    let mut smoke_dev = dev.htod_copy(smoke_host.into())?;
+    let mut new_smoke_dev = dev.htod_copy(new_smoke_host.into())?;
     let mut div_dev = dev.htod_copy(div_host.into())?;
     let mut pressure_a_dev = dev.htod_copy(pressure_a_host.into())?;
     let mut pressure_b_dev = dev.htod_copy(pressure_b_host.into())?;
@@ -46,6 +67,7 @@ fn main() -> Result<(), DriverError> {
         // Divergence
         let divergence = dev.get_func("fluid", "divergence").unwrap();
         divergence.launch(cfg, (&mut div_dev, &u_dev, &v_dev, ROWS, COLS))?;
+        dev.synchronize()?;
 
         // Pressure Jacobi
         for _ in 0..ITERATIONS {
@@ -61,16 +83,55 @@ fn main() -> Result<(), DriverError> {
                     COLS,
                 ),
             )?;
+            dev.synchronize()?;
 
             swap(&mut pressure_a_dev, &mut pressure_b_dev);
         }
 
         // Solve Incompressibility
         let incompress = dev.get_func("fluid", "incompress").unwrap();
-        incompress.launch(cfg, (&mut u_dev, &mut v_dev, &pressure_a_dev))?;
+        incompress.launch(cfg, (&mut u_dev, &mut v_dev, &pressure_a_dev, ROWS, COLS))?;
+        dev.synchronize()?;
+
+        // Advect velocity
+        let advect_velocity = dev.get_func("fluid", "advect_velocity").unwrap();
+        advect_velocity.launch(
+            cfg,
+            (
+                &u_dev,
+                &v_dev,
+                &mut new_u_dev,
+                &mut new_v_dev,
+                &smoke_dev,
+                DT,
+                H,
+                ROWS,
+                COLS,
+            ),
+        )?;
+        swap(&mut u_dev, &mut new_u_dev);
+        swap(&mut v_dev, &mut new_v_dev);
+        dev.synchronize()?;
+
+        // Advect smoke
+        let advect_smoke = dev.get_func("fluid", "advect_smoke").unwrap();
+        advect_smoke.launch(
+            cfg,
+            (
+                &smoke_dev,
+                &mut new_smoke_dev,
+                &u_dev,
+                &v_dev,
+                DT,
+                H,
+                ROWS,
+                COLS,
+            ),
+        )?;
+        swap(&mut smoke_dev, &mut new_smoke_dev);
     };
 
-    let result = dev.sync_reclaim(u_dev)?;
+    let result = dev.sync_reclaim(smoke_dev)?;
 
     for y in 0..ROWS {
         for x in 0..COLS {
